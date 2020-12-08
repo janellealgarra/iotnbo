c350e7aba91655cb93cbbbdd69d32b167ac477e0
Janelle Algarra <janellealgarra@janelles-mbp.lan>
2020-12-08 21:32:00 +0800

    completed assignment

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function scrollWin() {
-  window.scrollTo(0, 800);
+  window.scrollTo(0, 750);
+}
+
+function scrollWinTwo() {
+  window.scrollTo(0, 1000);
 }
 
 window.addEventListener('load', function(){
@@ -23,7 +27,6 @@ window.addEventListener('load', function(){
       }
   }
 
-
 let locGrid = document.getElementById('locationGrid');
 let proGrid = document.getElementById('professionalGrid');
 
@@ -34,7 +37,8 @@ let resourceHelp = [
     "location" : "Mandaluyong, Metro Manila",
     "hours": "open 24/7",
     "number": "(02) 8531 9001",
-    "link": "http://www.ncmh.gov.ph/"
+    "link": "http://www.ncmh.gov.ph/",
+    "web": "website"
   },
 
   {
@@ -43,7 +47,8 @@ let resourceHelp = [
     "location" : "Quezon City, Metro Manila",
     "hours": "Monday-Sunday, 9am-10pm",
     "number": "0917 511 7582",
-    "link": "https://www.facebook.com/ExcellentCommunityCareCenter/"
+    "link": "https://www.facebook.com/ExcellentCommunityCareCenter/",
+    "web": "website"
   },
 
   {
@@ -52,7 +57,8 @@ let resourceHelp = [
     "location" : "Quezon City, Metro Manila",
     "hours": "open 24/7",
     "number": "0917 520 3509",
-    "link": "https://www.lifechangerecoverycenter.com/"
+    "link": "https://www.lifechangerecoverycenter.com/",
+    "web": "website"
   },
 
   {
@@ -61,7 +67,8 @@ let resourceHelp = [
     "location" : "Quezon City, Metro Manila",
     "hours": "Monday-Sunday, 7–11:30am // 12:30–4pm",
     "number": "(02) 8921 4958",
-    "link": "https://pmha.org.ph/"
+    "link": "https://pmha.org.ph/",
+    "web": "website"
   },
 
   {
@@ -70,7 +77,8 @@ let resourceHelp = [
     "location" : "Lungsod Quezon, Kalakhang Maynila",
     "hours": "*please call for hours*",
     "number": "(02) 3434 4321",
-    "link": "https://www.webbline.com/mental-health/st-joseph-psychiatric-and-custodial-homecare-rev8312019303/"
+    "link": "https://www.webbline.com/mental-health/st-joseph-psychiatric-and-custodial-homecare-rev8312019303/",
+    "web": "website"
   },
 
   {
@@ -79,7 +87,8 @@ let resourceHelp = [
     "location" : "Makati, Metro Manila",
     "hours": "Monday-Sunday, 9am-5pm",
     "number": "(02) 8893 1893",
-    "link": "https://www.in-touch.org/"
+    "link": "https://www.in-touch.org/",
+    "web": "website"
   },
 
   {
@@ -88,7 +97,8 @@ let resourceHelp = [
     "location" : "Makati, Metro Manila",
     "hours": "Monday-Sunday, 8am-9pm",
     "number": "(02) 7966 7110",
-    "link": "https://prescriptionpsychiatrists.com.ph/"
+    "link": "https://prescriptionpsychiatrists.com.ph/",
+    "web": "website"
   },
 
   {
@@ -97,7 +107,8 @@ let resourceHelp = [
     "location" : "Mandaluyong, Metro Manila",
     "hours": "open 24/7",
     "number": "(02) 8401 2645",
-    "link": "http://www.divinemercyhomecare.com/"
+    "link": "http://www.divinemercyhomecare.com/",
+    "web": "website"
   },
 
   {
@@ -106,7 +117,8 @@ let resourceHelp = [
     "location" : "Manila, Metro Manila",
     "hours": "open 24/7",
     "number": "(02) 8554 8400",
-    "link": "https://www.pgh.gov.ph/"
+    "link": "https://www.pgh.gov.ph/",
+    "web": "website"
   },
 
   {
@@ -115,7 +127,8 @@ let resourceHelp = [
     "location" : "Muntinlupa, Metro Manila",
     "hours": "open 24/7",
     "number": "(02) 8771 9000",
-    "link": "https://www.asianhospital.com/"
+    "link": "https://www.asianhospital.com/",
+    "web": "website"
   },
 
   {
@@ -124,7 +137,8 @@ let resourceHelp = [
     "location" : "Taguig, Metro Manila",
     "hours": "open 24/7",
     "number": "(02) 8789 7700",
-    "link": "https://www.stlukes.com.ph/"
+    "link": "https://www.stlukes.com.ph/",
+    "web": "website"
   },
 
   {
@@ -133,7 +147,18 @@ let resourceHelp = [
     "location" : "Muntinlupa, Metro Manila",
     "hours": "Monday-Sunday, 8am-5pm",
     "number": "(02) 8772 2678",
-    "link": "https://www.tokyohealthlink.net/"
+    "link": "https://www.tokyohealthlink.net/",
+    "web": "website"
+  },
+
+  {
+    "category" : "pro",
+    "title" : "Dr. Rose Pugeda",
+    "location" : "Healthway Medical Clinic, 2nd Floor, Alabang Town Center, Alabang-Zapote Road, Muntinlupa, Metro Manila",
+    "hours": "Monday-Wednesday, 2pm-4pm",
+    "number" : "(02) 7772 3527",
+    "link": "https://www.healthway.com.ph/healthway-multi-specialty-center/doctors",
+    "web": "website"
   },
 
   {
@@ -141,7 +166,9 @@ let resourceHelp = [
     "title" : "Jay Madelon Castillo Carcereny, M.D.",
     "location" : "Healthway Medical Clinic, 2nd Floor, Alabang Town Center, Muntinlupa, Metro Manila",
     "hours": "*please schedule an appointment through the facility*",
-    "number" : "(02) 7751 4929"
+    "number" : "(02) 7751 4929",
+    "link": "https://www.healthway.com.ph/healthway-multi-specialty-center/doctors",
+    "web": "website"
   },
 
   {
@@ -149,7 +176,9 @@ let resourceHelp = [
     "title" : "Maria Madeline B. Mallillin, MD",
     "location" : "Room 705, Asian Hospital And Medical Center, Alabang, Muntinlupa",
     "hours": "*please schedule an appointment through the facility*",
-    "number" : "none"
+    "number" : "none",
+    "link": "https://www.asianhospital.com/",
+    "web": "website"
   },
 
   {
@@ -157,14 +186,19 @@ let resourceHelp = [
     "title" : "Dr. Salvador Benjamin D. Vista",
     "location" : "Room 201, Asian Hospital And Medical Center, Alabang, Muntinlupa",
     "hours": "*please schedule an appointment through the facility*",
-    "number" : "(02) 8771 9000"
+    "number" : "(02) 8771 9000",
+    "link": "https://www.asianhospital.com/",
+    "web": "website"
   },
 
   {
     "category" : "pro",
     "title" : "Lovie Hope Go-Chu, M.D.",
     "location" : "Room 215, 2nd Floor, Hall B, Tower 1, Makati Medical Center, Amorsolo Street, Makati City",
-    "hours" : "Monday-Sunday, 9am-5pm"
+    "hours" : "Monday-Sunday, 9am-5pm",
+    "number" : "n/a",
+    "link": "https://www.makatimed.net.ph/",
+    "web": "website"
   },
 
   {
@@ -172,7 +206,9 @@ let resourceHelp = [
     "title" : "Paul V. Lee, MD",
     "location" : "Room 22, Philippine Children's Medical Center, Quezon City, Metro Manila",
     "hours": "*please schedule an appointment through the facility*",
-    "number" : "(02) 8401 2911"
+    "number" : "(02) 8401 2911",
+    "link": "http://www.pcmc.gov.ph/",
+    "web": "website"
   },
 
   {
@@ -180,7 +216,9 @@ let resourceHelp = [
     "title" : "Dr. Nelson Anthony Mallillin",
     "location" : "Room 209, Our Lady of the Pillar Medical Center, Tamsui Avenue, Bayan Luma, Imus",
     "hours": "*please schedule an appointment through the facility*",
-    "number" : "(046) 472 3773"
+    "number" : "(046) 472 3773",
+    "link": "https://www.facebook.com/olpmc/",
+    "web": "website"
   },
 
   {
@@ -188,7 +226,9 @@ let resourceHelp = [
     "title" : "Dr Kenneth Javate",
     "location" : "Unit 308 The Xavier Residences, 41 Esteban Abada St, Quezon City, 1108 Metro Manila",
     "hours": "*please schedule an appointment through the facility*",
-    "number" : "0916 250 2040"
+    "number" : "0916 250 2040",
+    "link": "https://pssc.org.ph/",
+    "web": "website"
   },
 
   {
@@ -196,7 +236,9 @@ let resourceHelp = [
     "title" : "Dr. Melissa Reyes",
     "location" : "Room 210, 2nd Floor, Philippine Social Science Center, Commonwealth Avenue, Diliman, Quezon City, Metro Manila",
     "hours": "*please schedule an appointment through the facility*",
-    "number" : "(02) 3453 8257"
+    "number" : "(02) 3453 8257",
+    "link": "https://pssc.org.ph/",
+    "web": "website"
   },
 
   {
@@ -204,7 +246,9 @@ let resourceHelp = [
     "title" : "Elizabeth Rondain, M.D.",
     "location" : " Room 121, 1st Floor, Hall C, Tower 1, Makati Medical Center, Amorsolo Street, Makati City",
     "hours": "*please schedule an appointment in person*",
-    "number" : "none"
+    "number" : "n/a",
+    "link": "https://www.makatimed.net.ph/",
+    "web": "website"
   },
 
   {
@@ -212,7 +256,9 @@ let resourceHelp = [
     "title" : "Dr. Margaret Helen Udarbe-Alvarez",
     "location" : "Room 210, 2nd Floor, PSS Center, Commonwealth Avenue, Diliman, Quezon City, Metro Manila",
     "hours": "*please schedule an appointment through the facility*",
-    "number" : "(02) 3453 8257"
+    "number" : "(02) 3453 8257",
+    "link": "https://pscentre.org/",
+    "web": "website"
   },
 
   {
@@ -220,15 +266,9 @@ let resourceHelp = [
     "title" : "Dr. Josephine Mary Ganar",
     "location" : "Healthway Medical Clinic, 2nd Floor, Alabang Town Center, Alabang-Zapote Road, Muntinlupa, Metro Manila",
     "hours": "*please schedule an appointment through the facility*",
-    "number" : "(02) 7720 6102"
-  },
-
-  {
-    "category" : "pro",
-    "title" : "Dr. Rose Daryl",
-    "location" : "Alabang",
-    "hours": "Monday-Thursday, 1pm-5pm",
-    "number" : "(02) dd"
+    "number" : "(02) 7720 6102",
+    "link": "https://www.healthway.com.ph/healthway-multi-specialty-center/doctors",
+    "web": "website"
   }
 
 ];
@@ -243,6 +283,9 @@ for (let i = 0; i < resourceHelp.length; i++) {
 function createElement(incomingJSON){
   let newContentElement = document.createElement("DIV");
   newContentElement.classList.add('contentItem');
+
+  let newContentLink = document.createElement("A");
+  newContentLink.href = incomingJSON['link'];
 
   let newContentTitle = document.createElement("H4");
   newContentTitle.classList.add('contentTitle');
@@ -263,6 +306,14 @@ function createElement(incomingJSON){
   newContentNumber.classList.add('contentNumber');
   newContentNumber.innerText = incomingJSON['number'];
   newContentElement.appendChild(newContentNumber);
+
+  let newWebLink = document.createElement("P");
+  newWebLink.classList.add('webLink');
+  newWebLink.innerText = incomingJSON['web'];
+  newContentElement.appendChild(newWebLink);
+  newContentLink.appendChild(newWebLink);
+  newContentElement.appendChild(newContentLink);
+
 
   if (incomingJSON['category'] === 'loc'){
     locGrid.appendChild(newContentElement);
